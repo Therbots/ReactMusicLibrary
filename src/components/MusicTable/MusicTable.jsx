0c2345fcfd4de7f1cbb2117e5edc2d660da30cd5
taylor herbots <therbots@live.com>
2021-09-13 12:39:28 -0500

    added table header to mucistable.jsx and empty song array to app.jsx

--- src/components/MusicTable/MusicTable.jsx
+++ src/components/MusicTable/MusicTable.jsx
@@ -4,6 +4,12 @@ import './MusicTable.css';
 const MusicTable = (props) => {
     return (
         <table>
+            <tr>
+                <th>Title</th>
+                <th>Artist</th>
+                <th>Album</th>
+                <th>Release Date</th>
+            </tr>
             {props.music.map((item => 
                 <tr>
                     <td key={item.id}>{item.title}</td>
