f2b08c37e0a624c5ecafdc8137296183ee7579d7
taylor herbots <therbots@live.com>
2021-09-13 11:03:26 -0500

    installed axios to connect django music library

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {  }
     }
+
+    componentDidMount() {
+        this.getAllSongs();
+    }
+
+    async getAllSongs() {
+        let response = await axios.get('http://127.0.0.1:8000/music/');
+        this.setState({
+            songs: response.data
+        })
+    }
+
     render() { 
         return ( 
             <h1>hello world!</h1>
          );
     }
 }
+
+    
  
 export default App;
